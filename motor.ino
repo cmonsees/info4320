30b93055f29df829bf03dd3f52d7d2b9f6037c1b
Conner Monsees <conner.monsees@gmail.com>
2018-05-12 00:43:27 -0400

    Motor functions
    asdfas

--- motor.ino
+++ motor.ino
@@ -2,14 +2,22 @@
 const int motorPin[3] = {D0, D3, A6};
 const int leftPin[3] = {D2, D5, A4};
 const int rightPin[3] = {D1, D4, A5};
+int motorCounter[3] = {0, 0, 0};
+
+const int petalSPin = A0;
+const int petalDPin = A1;
+const int STEPS_PER_TURN = 200;
+const int delay_between_step_microsec = 5000;
 
 const int Blue_PIN = D6;
 const int Pink_PIN = D7;
 int incomingByte; // a variable to read incoming serial data into
- 
+
 const int B_PIN = A3;
 const float VCC = 4.98; // Measured voltage of Ardunio 5V line
 const float R_DIV = 3230.0; // Measured resistance of 3.3k
+
+int spoolToggle = 1; // global spool direction
 
 int calcForce(int input) {
   const float VCC = 3.3; // Measured voltage of Ardunio 5V line
@@ -23,56 +31,70 @@ int calcForce(int input) {
     force = (fsrG - 0.00075) / 0.00000032639;
   } else { force =  fsrG / 0.000000642857; }
 
-  int returnSpeed = map(force, 1, 1000, 0, 128); // Map force to motor Speed
+  int returnSpeed = map(force, 1, 250, 0, 128); // Map force to motor Speed
   if (abs(returnSpeed) > 128) { returnSpeed = 128; }
 
   return returnSpeed;
 }
 
+void step(bool forward) {
+  if (forward == true) {
+    digitalWrite(petalDPin, HIGH);
+  } else {
+    digitalWrite(petalDPin, LOW);
+  }
+  // creating a step
+  digitalWrite(petalSPin, HIGH);
+  // minimum delay is 1.9us
+  digitalWrite(petalSPin, LOW);
+}
+
+void petalSteps(int number_of_steps) {
+  Serial.write("Steps called\n");
+  bool move_forward = true;
+  // Establishing the direction
+  if (number_of_steps >= 0) {
+    move_forward = true;
+  } else {
+    move_forward = false;
+    number_of_steps = -number_of_steps;
+  }
+  for (int i = 0; i < number_of_steps; i++) {
+    step(move_forward);
+    // Delay for proper speed
+    delayMicroseconds(delay_between_step_microsec);
+  }
+}
+
 void spool(int motor, int speed, int direction) {
   if ((direction == 1) || (direction == -1)) {
+    digitalWrite(motorPin[motor], LOW);
+    // if (motorCounter[motor] >= 3) {
+    //   spoolToggle = -spoolToggle;
+    //   motorCounter[motor]--;
+    //   // spool(motor, speed, spoolToggle);
+    // }
+    // if (motorCounter[motor] < 0) {
+    //   spoolToggle = spoolToggle;
+    //   motorCounter[motor]++;
+    //   // spool(motor, speed, spoolToggle);
+    // }
     if (direction == 1) { // 1 = spool
       digitalWrite(leftPin[motor], HIGH);
       digitalWrite(rightPin[motor], LOW);
+      motorCounter[motor]++;
       analogWrite(motorPin[motor], speed);
     }
     if (direction == -1) { // -1 = unspool
       digitalWrite(leftPin[motor], LOW);
       digitalWrite(rightPin[motor], HIGH);
+      motorCounter[motor]--;
       analogWrite(motorPin[motor], speed);
     }
+    delay(150);
+    digitalWrite(motorPin[motor], LOW);
   }
 }
-
-void spoolEase(int motor, int direction) {
-  if (direction == 1) { // 1 = spool
-    digitalWrite(leftPin[motor], HIGH);
-    digitalWrite(rightPin[motor], LOW);
-    for (int e = 1; e < 128; e++) {
-      analogWrite(motorPin[motor], e);
-      e = e*2;
-      delay(1);
-    }
-    for (int o = 1; o < 128; o++) {
-      analogWrite(motorPin[motor], o);
-      o = o/2;
-      delay(1);
-    }
-  }
-  if (direction == -1) { // -1 = unspool
-    digitalWrite(leftPin[motor], LOW);
-    digitalWrite(rightPin[motor], HIGH);
-    for (int e = 1; e < 128; e*2) {
-      analogWrite(motorPin[motor], e);
-    }
-    for (int o = 1; o < 128; o/2) {
-      analogWrite(motorPin[motor], o);
-    }
-  }
-  delay(100);
-}
-
-int spoolToggle = 1; // global spool direction
 
 void setup() {
   Serial.begin(9600);
@@ -84,55 +106,63 @@ void setup() {
     pinMode(rightPin[i], OUTPUT);
 
     // set motors to default spool state
-    spool(motorPin[i], 0, 1); // 1 = spool; -1 = unspool
+    digitalWrite(motorPin[i], LOW); // 1 = spool; -1 = unspool
   }
 
   pinMode(B_PIN, INPUT);
   pinMode(Blue_PIN, OUTPUT);
   pinMode(Pink_PIN, OUTPUT);
+
+  pinMode(petalSPin, OUTPUT);
+  pinMode(petalDPin, OUTPUT);
 }
 
 void loop(){
-
   if(Serial1.available() >0) { //Lights for A
     incomingByte = Serial1.read();
-    if(incomingByte == 'B') {digitalWrite(Blue_PIN, HIGH); delay(1000); digitalWrite(Blue_PIN, LOW);}
+    if ((incomingByte == 'c') || (incomingByte == 'C')) {
+      Serial.write("C received\n");
+      spool(0, 100, -spoolToggle);
+    }
+    if ((incomingByte == 'd') || (incomingByte == 'D')) {
+      Serial.write("D received\n");
+      spool(0, 100, -spoolToggle);
+      spool(1, 100, -spoolToggle);
+    }
+    if ((incomingByte == 'e') || (incomingByte == 'E')) {
+      Serial.write("E received\n");
+      spool(1, 100, -spoolToggle);
+    }
+    if ((incomingByte == 'F') || (incomingByte == 'f')) {
+      Serial.write("Direction toggled\n");
+      spoolToggle = -spoolToggle;
+      //spool(1, 25, -spoolToggle);
+      //spool(2, 25, -spoolToggle);
+    }
+    if ((incomingByte == 'G') || (incomingByte == 'g')) {
+      Serial.write("G received\n");
+      spool(2, 100, -spoolToggle);
+    }
+    if (incomingByte == '$') {
+      Serial.write("A received\n");
+      // spool(2, 25, spoolToggle);
+      // spool(0, 25, -spoolToggle);
+      petalSteps(200);
+      digitalWrite(Blue_PIN, HIGH);
+      digitalWrite(Blue_PIN, LOW);
+    }
+
   }
-  
+
   int B_read = analogRead(B_PIN);
   int B_force = calcForce(B_read);
-  
-  if(B_force > 50 && B_force < 100) {
-  digitalWrite(Pink_PIN, HIGH); Serial.print('4'); Serial1.print('4'); delay(1000); digitalWrite(Pink_PIN, LOW);}
-  if(B_force > 100) {
-  digitalWrite(Pink_PIN, HIGH);Serial1.print('5'); delay(1000); digitalWrite(Pink_PIN, LOW);}
-  
-//  int C_Read = analogRead(C_Pin);
-//  int D_Read = analogRead(D_Pin);
-//  int E_Read = analogRead(E_Pin);
-//  int F_Read = analogRead(F_Pin);
-//
-//  if (C_Read > 50) { // 50 provides padding for signal noise
-//    int motorSpeed = calcForce(C_Read);
-//    Serial.print(String(motorSpeed)+"\n");
-//    // delay(5);
-//    spool(0, motorSpeed, spoolToggle);
-//    // Serial.print("Read C");
-//    // spoolEase(0, spoolToggle);
-//  }
-//
-//  if (D_Read > 50) { // 50 provides padding for signal noise
-//    int motorSpeed2 = calcForce(D_Read);
-//    spool(1, motorSpeed2, spoolToggle);
-//  }
-//
-//  if (E_Read > 50) { // 50 provides padding for signal noise
-//    int motorSpeed3 = calcForce(E_Read);
-//    spool(2, motorSpeed3, spoolToggle);
-//  }
-//  if (F_Read > 500) {
-//    spoolToggle = -spoolToggle;
-//    delay(300);
-//  }
-
+  if(B_force > 50) {
+    Serial.write("B received\n");
+    // spool(0, 25, spoolToggle);
+    petalSteps(-200);
+    digitalWrite(Pink_PIN, HIGH);
+    Serial.print('b');
+    Serial1.print('b');
+    digitalWrite(Pink_PIN, LOW);
+  }
 }
